4378c50a25faa61bc09d055f341dbacdf77dd4a5
Alexander <knibz@protonmail.com>
2022-06-29 23:46:53 -0400

    edit dictionary from strings to chars, move dictionary to Scorer file

--- Scrabble.Solutions/Program.cs
+++ Scrabble.Solutions/Program.cs
@@ -1,30 +1,59 @@
 using System;
-using System.Collections.Generic;
+
 
 namespace ScrabbleScore
 {
   public class Program
   {
-   public static Dictionary<string, int> letterValues = new Dictionary<string, int>()
-   {{"A", 1}, {"E", 1}, {"I", 1}, {"O", 1}, {"U", 1}, {"L", 1}, {"N", 1}, {"R", 1}, {"S", 1}, {"T", 1}, {"D", 2}, {"G", 2}, {"B", 3}, {"C", 3}, {"M", 3}, {"P", 3}, {"F", 4}, {"H", 4}, {"V", 4}, {"W", 4}, {"Y", 4}, {"K", 5}, {"J", 8}, {"X", 8}, {"Q", 10}, {"Z", 10}};
-   
+
    public static void Main() 
    {
     Console.WriteLine("Welcome to the premier Scrabble score checker!");
+    UserInput();
+   }
+
+   public static void UserInput()
+   {
     Console.WriteLine("Please enter a word to see the Scrabble value!");
-    string userInput = Console.ReadLine();
+    string inputWord = Console.ReadLine();
+    if (letterCheck(inputWord) == true)
+    {
+      int wordResult = Scorer.ScoreTotal(inputWord.ToUpper());
+      Console.WriteLine("The word " + inputWord + " is worth: " + wordResult + " points!");
+      UserInput();
+    }
+    else
+    {
+      Console.WriteLine("Please make sure to enter a word without spaces, numbers, or special characters! Valid Scrabble words only!");
+      UserInput();
+    }
+   }
 
-    char[] userWordArray = userInput.ToCharArray();
-    foreach (var word in userWordArray)
+   public static bool letterCheck(string inputWord)
+   {
+    foreach (char letter in inputWord)
     {
-     Console.WriteLine(word);
+      if (char.IsLetter(letter) != true)
+      {
+        return false;
+      }
     }
-    
-    static void SplitWordValue()
-    {
-      //new code
-    }
+      return true;
+   }
   }
+}
 
-}
-}
+
+// char[] userWordArray = userInput.ToCharArray();
+//     bool charFind = userWordArray.Contains();
+//     foreach (var word in userWordArray)
+//     {
+//      Console.WriteLine(word);
+//     }
+
+// foreach( KeyValuePair<string, int> kvPair in letterValues)
+    
+    // static void SplitWordValue()
+    // {
+      
+    // }
